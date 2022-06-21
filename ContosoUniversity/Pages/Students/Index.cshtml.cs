2af34e6286fea44e017a8a242a46fbc3712abaed
Hasnae-bouhmady <hasnaebouhmady@student.emi.ac.ma>
2022-06-21 20:27:09 +0100

    update index.cshtml.cs

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -1,4 +1,16 @@
-public class IndexModel : PageModel
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Students
+{
+    public class IndexModel : PageModel
 {
     private readonly SchoolContext _context;
     public IndexModel(SchoolContext context)
@@ -40,4 +52,5 @@ public class IndexModel : PageModel
 
         Students = await studentsIQ.AsNoTracking().ToListAsync();
     }
+}
 }
