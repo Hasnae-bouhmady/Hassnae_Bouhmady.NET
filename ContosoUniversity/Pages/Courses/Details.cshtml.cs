eb07fdbabfda8f5dc60d1011a0b954f4df063d7a
Hasnae-bouhmady <hasnaebouhmady@student.emi.ac.ma>
2022-06-21 21:17:23 +0100

    Update Related Data

--- ContosoUniversity/Pages/Courses/Details.cshtml.cs
+++ ContosoUniversity/Pages/Courses/Details.cshtml.cs
@@ -21,23 +21,23 @@ namespace ContosoUniversity.Pages.Courses
 
       public Course Course { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null || _context.Courses == null)
-            {
-                return NotFound();
-            }
+       public async Task<IActionResult> OnGetAsync(int? id)
+{
+    if (id == null)
+    {
+        return NotFound();
+    }
 
-            var course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseID == id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                Course = course;
-            }
-            return Page();
-        }
+    Course = await _context.Courses
+        .AsNoTracking()
+        .Include(c => c.Department)
+        .FirstOrDefaultAsync(m => m.CourseID == id);
+
+    if (Course == null)
+    {
+        return NotFound();
+    }
+    return Page();
+}
     }
 }
